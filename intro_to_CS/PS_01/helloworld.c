61337d04b66e5721f488a48439579bfd8e01770c
Harry Lanz <lanzharry97@gmail.com>
2025-03-05 23:28:16 +0000

    edit comment to be more concise

--- intro_to_CS/PS_01/helloworld.c
+++ intro_to_CS/PS_01/helloworld.c
@@ -5,6 +5,6 @@ int main(void)
     // Print Hello, World! to terminal
     printf("Hello, World!\n");
 
-    // Return 0 as is common practice - unnecessary with modern compilers but it is good practice
+    // Return 0 - unnecessary with modern compilers but it is good practice
     return 0;
 }
